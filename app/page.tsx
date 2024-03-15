2fb03f559099002acad1d3870d37724fcfddb384
Fillip Mangia <fillipgms@gmail.com>
2024-03-15 08:08:02 -0300

    generations

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,17 @@ import AllPokemon from "@/components/AllPokemon";
 import HomeLoading from "@/components/HomeLoading";
 import { Suspense } from "react";
 
-export default function Home() {
+export default function Home({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | string[] | undefined };
+}) {
+    const gen = searchParams["gen"] ?? "1";
+
     return (
         <main>
             <Suspense fallback={<HomeLoading />}>
-                <AllPokemon />
+                <AllPokemon gen={gen} />
             </Suspense>
         </main>
     );
--- components/AllPokemon.tsx
+++ components/AllPokemon.tsx
@@ -2,9 +2,33 @@ import Image from "next/image";
 import PokeAPI from "pokedex-promise-v2";
 import React from "react";
 
-const AllPokemon = async () => {
+interface AllPokemonInfo {
+    gen: string | string[];
+}
+
+const genCount = {
+    1: { count: 151, offset: 0 },
+    2: { count: 100, offset: 151 },
+    3: { count: 135, offset: 251 },
+    4: { count: 107, offset: 386 },
+    5: { count: 156, offset: 493 },
+    6: { count: 72, offset: 649 },
+    7: { count: 88, offset: 721 },
+    8: { count: 96, offset: 809 },
+    9: { count: 120, offset: 905 },
+};
+
+const AllPokemon = async (gen: AllPokemonInfo) => {
     const baseUrl = "https://pokeapi.co/api/v2/";
-    const res = await fetch(`${baseUrl}pokemon?limit=151`);
+    const parseGen = Array.isArray(gen.gen)
+        ? parseInt(gen.gen[0])
+        : parseInt(gen.gen as string);
+
+    const res = await fetch(
+        `${baseUrl}pokemon?limit=${
+            genCount[parseGen as keyof typeof genCount].count
+        }&offset=${genCount[parseGen as keyof typeof genCount].offset}`
+    );
     const data = await res.json();
 
     const pokemonInfoPromises = data.results.map(
